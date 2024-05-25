204a1078a530c96b3ab52d81edca143d4657df0c
Heclon <1255018+heclon@users.noreply.github.com>
2024-05-25 16:08:28 +1000

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import "@/styles/app.css";
+import type { AppProps } from "next/app";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(outputs);
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
