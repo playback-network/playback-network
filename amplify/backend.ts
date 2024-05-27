30b9f44ed07fc617c35264eec4bb581245ff6013
heclon <1255018+heclon@users.noreply.github.com>
2024-05-28 01:30:30 +1000

    Removed Auth from backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,19 +2,15 @@ import { defineBackend } from '@aws-amplify/backend';
 import { Stack } from 'aws-cdk-lib';
 import {
   AuthorizationType,
-  CognitoUserPoolsAuthorizer,
   Cors,
   LambdaIntegration,
   RestApi,
 } from 'aws-cdk-lib/aws-apigateway';
-import { Policy, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { myApiFunction } from './functions/api-function/resource';
-import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
 
 const backend = defineBackend({
-  auth,
   data,
   myApiFunction,
   storage,
@@ -49,36 +45,8 @@ const itemsPath = myRestApi.root.addResource('items', {
 // add methods you would like to create to the resource path
 itemsPath.addMethod('GET', lambdaIntegration);
 itemsPath.addMethod('POST', lambdaIntegration);
-itemsPath.addMethod("DELETE", lambdaIntegration);
+itemsPath.addMethod('DELETE', lambdaIntegration);
 itemsPath.addMethod('PUT', lambdaIntegration);
-
-// // add a proxy resource path to the API
-// itemsPath.addProxy({
-//   anyMethod: true,
-//   defaultIntegration: lambdaIntegration,
-// });
-
-
-// // create a new IAM policy to allow Invoke access to the API
-// const apiRestPolicy = new Policy(apiStack, 'RestApiPolicy', {
-//   statements: [
-//     new PolicyStatement({
-//       actions: ['execute-api:Invoke'],
-//       resources: [
-//         `${myRestApi.arnForExecuteApi('items')}`,
-//         `${myRestApi.arnForExecuteApi('cognito-auth-path')}`,
-//       ],
-//     }),
-//   ],
-// });
-
-// // attach the policy to the authenticated and unauthenticated IAM roles
-// backend.auth.resources.authenticatedUserIamRole.attachInlinePolicy(
-//   apiRestPolicy
-// );
-// backend.auth.resources.unauthenticatedUserIamRole.attachInlinePolicy(
-//   apiRestPolicy
-// );
 
 // add outputs to the configuration file
 backend.addOutput({
