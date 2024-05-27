5403bbae02680b0b5269cca8b856629cee9f6412
heclon <1255018+heclon@users.noreply.github.com>
2024-05-28 00:23:11 +1000

    Set permissons to S3 Bucket and added fileUpload ui component

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,5 +1,17 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'medias'
+  name: 'medias',
+  access: (allow) => ({
+    'submitted/{entity_id}/*': [
+      // anyone can submit files:
+      allow.guest.to(['read', 'write']),
+
+      // only owners can delete theirs:
+      allow.entity('identity').to(['read', 'write', 'delete']),
+
+      // admins can also delete:
+      allow.authenticated.to(['read', 'write', 'delete']),
+    ],
+  }),
 });
