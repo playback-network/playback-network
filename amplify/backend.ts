440b69b7583be35a701052b99482e063cc847bf2
heclon <1255018+heclon@users.noreply.github.com>
2024-06-02 04:17:06 +1000

    try fix configuring in console

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { EventSourceMapping, StartingPosition } from 'aws-cdk-lib/aws-lambda';
-// import { DynamoEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
+import { DynamoEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { payloadmanagerFunction } from './functions/payloadmanager-function/resource';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
@@ -17,27 +17,30 @@ const backend = defineBackend({
   data,
 });
 
-const taskTable = backend.data.resources.tables['Task'];
+// const taskTable = backend.data.resources.tables['Task'];
+// if (!taskTable.tableStreamArn) {
+//   throw new Error('The DynamoDB table stream ARN is not defined');
+// }
 
-backend.myDynamoDBFunction.resources.lambda.role?.attachInlinePolicy(
-  new Policy(Stack.of(taskTable), 'DynamoDBPolicy', {
-    statements: [
-      new PolicyStatement({
-        effect: Effect.ALLOW,
-        actions: [
-          'dynamodb:DescribeStream',
-          'dynamodb:GetRecords',
-          'dynamodb:GetShardIterator',
-          'dynamodb:ListStreams',
-        ],
-        resources: [backend.data.resources.tables['Task'].tableArn],
-      }),
-    ],
-  })
-);
+// backend.myDynamoDBFunction.resources.lambda.role?.attachInlinePolicy(
+//   new Policy(Stack.of(taskTable), 'DynamoDBPolicy', {
+//     statements: [
+//       new PolicyStatement({
+//         effect: Effect.ALLOW,
+//         actions: [
+//           'dynamodb:DescribeStream',
+//           'dynamodb:GetRecords',
+//           'dynamodb:GetShardIterator',
+//           'dynamodb:ListStreams',
+//         ],
+//         resources: [taskTable.tableStreamArn], // Use tableStreamArn to specify the stream ARN
+//       }),
+//     ],
+//   })
+// );
 
-new EventSourceMapping(Stack.of(taskTable), 'dynamoDB-function', {
-  target: backend.myDynamoDBFunction.resources.lambda,
-  eventSourceArn: taskTable.tableStreamArn,
-  startingPosition: StartingPosition.LATEST,
-});
+// new EventSourceMapping(Stack.of(taskTable), 'DynamoDBEventSourceMapping', {
+//   target: backend.myDynamoDBFunction.resources.lambda,
+//   eventSourceArn: taskTable.tableStreamArn,
+//   startingPosition: StartingPosition.LATEST,
+// });
