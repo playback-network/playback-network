77e005d332b44dddd8c877b00836814b43e7f4e7
heclon <1255018+heclon@users.noreply.github.com>
2024-06-01 20:31:05 +1000

    Cleaned up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,6 @@ import outputs from '@/amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
 
 Amplify.configure(outputs);
-const existingConfig = Amplify.getConfig();
-Amplify.configure({
-  ...existingConfig,
-  API: {
-    REST: outputs.custom.API,
-  },
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
