318d487d88368a88d0e18b856c763d074eaaa0b3
heclon <1255018+heclon@users.noreply.github.com>
2024-06-02 13:52:27 +1000

    Added sendTransaction function to galadriel lambda

--- amplify/storage/on-upload-handler.ts
+++ amplify/storage/on-upload-handler.ts
@@ -1,4 +1,37 @@
 import { S3Handler } from 'aws-lambda';
+import { ethers } from 'ethers';
+
+// Placeholder ABI
+const contractABI: any = [
+  // Replace this with the actual ABI
+  'function startChat(address recipient, string systemMessage, string message, string[] imageUrls)',
+];
+
+async function sendTransaction(
+  recipient: string,
+  systemMessage: string,
+  message: string,
+  imageUrls: string[]
+) {
+  const contractAddress = '0x2dC8Bc53ECf1A59188e4c7fAB0c7bB57339F85e7';
+  const privateKey =
+    '0xe11dfd3f6a929f1e6e3374f19c15aa89d83d800cc5ce71ef5b385db78820d26e';
+
+  const provider = new ethers.JsonRpcProvider('https://devnet.galadriel.com');
+  const signer = new ethers.Wallet(privateKey, provider);
+
+  const contract = new ethers.Contract(contractAddress, contractABI, signer);
+
+  const tx = await contract.startChat(
+    recipient,
+    systemMessage,
+    message,
+    imageUrls
+  );
+  await tx.wait();
+
+  console.log('Transaction Hash:', tx.hash);
+}
 
 export const handler: S3Handler = async (event) => {
   const objectKeys = event.Records.map((record) => record.s3.object.key);
